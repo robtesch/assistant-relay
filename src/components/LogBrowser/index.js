53bb5a40a4081d380765b6fd3838b8b3afab11a3
Robert Teschmacher <r.teschmacher@gmail.com>
2021-08-12 12:37:08 +0100

    npm update and initial attempt at cleanup

--- src/components/LogBrowser/index.js
+++ src/components/LogBrowser/index.js
@@ -1,40 +1,40 @@
-import { useEffect, useState } from 'react';
-import moment from 'moment';
-import { post } from '../../helpers/api';
-import Filters from './filters';
+import { useEffect, useState } from "react";
+import moment from "moment";
+import { post } from "../../helpers/api";
+import Filters from "./filters";
 
 function LogBrowser() {
   const [loading, setLoading] = useState(true);
   const [logs, setLogs] = useState([]);
   const [rowCount, setRowCount] = useState(10);
-  const [order, setOrder] = useState('desc');
-  const [logLevel, setLogLevel] = useState('all');
-  const [serviceLevel, setServiceLevel] = useState('all');
+  const [order, setOrder] = useState("desc");
+  const [logLevel, setLogLevel] = useState("all");
+  const [serviceLevel, setServiceLevel] = useState("all");
 
   useEffect(() => {
     async function getLogs() {
       setLoading(true);
       try {
-        const { data } = await post('/api/server/getLogs', {
+        const { data } = await post("/api/server/getLogs", {
           options: {
             limit: rowCount,
-            order: order,
+            order: order
           },
           filters: {
             level: logLevel,
-            service: serviceLevel,
-          },
+            service: serviceLevel
+          }
         });
 
         setLogs(data);
         setLoading(false);
       } catch (e) {
         // TODO:  Trigger UI Alert
-        await post('/api/server/writeLogs', {
-          level: 'error',
+        await post("/api/server/writeLogs", {
+          level: "error",
           message: e.message,
-          service: 'web',
-          func: 'LogBrowser - getLogs',
+          service: "web",
+          func: "LogBrowser - getLogs"
         });
       }
       setLoading(false);
@@ -45,61 +45,55 @@ function LogBrowser() {
 
   function textClass(i) {
     switch (i) {
-      case 'silly':
-        return 'text-pink-400';
-      case 'debug':
-        return 'text-gray-400';
-      case 'verbose':
-        return 'text-green-400';
-      case 'info':
-        return 'text-teal-400';
-      case 'warn':
-        return 'text-orange-400';
-      case 'error':
-        return 'text-red-400';
+      case "silly":
+        return "text-pink-400";
+      case "debug":
+        return "text-gray-400";
+      case "verbose":
+        return "text-green-400";
+      case "info":
+        return "text-teal-400";
+      case "warn":
+        return "text-orange-400";
+      case "error":
+        return "text-red-400";
       default:
-        return 'text-gray-400';
+        return "text-gray-400";
     }
   }
 
   function serviceClass(i) {
     switch (i) {
-      case 'server':
-        return 'text-blue-400';
-      case 'web':
-        return 'text-purple-400';
-      case 'assistant':
-        return 'text-green-400';
+      case "server":
+        return "text-blue-400";
+      case "web":
+        return "text-purple-400";
+      case "assistant":
+        return "text-green-400";
       default:
-        return 'text-gray-400';
+        return "text-gray-400";
     }
   }
 
-  function Row(row) {
+  function Row(row, index) {
     return (
-      <tr>
-        <td
-          className={`px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium ${textClass(
-            row.level,
-          )}`}
-        >
+      <tr key={index}>
+        <td className={`px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium ${textClass(
+          row.level
+        )}`}>
           {row.level}
         </td>
-        <td
-          className={`px-6 py-4 whitespace-no-wrap text-sm leading-5 ${serviceClass(row.service)}`}
-        >
+        <td className={`px-6 py-4 whitespace-no-wrap text-sm leading-5 ${serviceClass(row.service)}`}>
           {row.service}
         </td>
-        <td
-          className={`px-6 py-4 whitespace-no-wrap text-sm leading-5 ${serviceClass(row.service)}`}
-        >
+        <td className={`px-6 py-4 whitespace-no-wrap text-sm leading-5 ${serviceClass(row.service)}`}>
           {row.func}
         </td>
         <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
           {row.message}
         </td>
         <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">
-          {moment(row.timestamp).format('MMM Do, HH:mm')}
+          {moment(row.timestamp).format("MMM Do, HH:mm")}
         </td>
       </tr>
     );
@@ -107,50 +101,46 @@ function LogBrowser() {
 
   return (
     <div>
-      <Filters
-        rowCount={i => setRowCount(i)}
-        logOrder={i => setOrder(i)}
-        logLevel={i => setLogLevel(i)}
-        serviceLevel={i => setServiceLevel(i)}
-      />
+      <Filters rowCount={i => setRowCount(i)}
+               logOrder={i => setOrder(i)}
+               logLevel={i => setLogLevel(i)}
+               serviceLevel={i => setServiceLevel(i)}/>
       <div className="flex flex-col rounded-lg shadow-lg">
         <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
           <div className="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
             <table className="min-w-full divide-y divide-gray-200">
               <thead>
-                <tr>
-                  <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                    Level
-                  </th>
-                  <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                    Service
-                  </th>
-                  <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                    Function
-                  </th>
-                  <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                    Message
-                  </th>
-                  <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                    Timestamp
-                  </th>
-                </tr>
+              <tr>
+                <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  Level
+                </th>
+                <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  Service
+                </th>
+                <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  Function
+                </th>
+                <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  Message
+                </th>
+                <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  Timestamp
+                </th>
+              </tr>
               </thead>
               <tbody className="bg-white divide-y divide-gray-200">
-                {logs.length > 0 ? (
-                  logs.map(r => {
-                    return Row(r);
-                  })
-                ) : (
-                  <tr>
-                    <td
-                      colspan="5"
-                      className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500 text-center"
-                    >
-                      No Logs
-                    </td>
-                  </tr>
-                )}
+              {logs.length > 0 ? (
+                logs.map((r, index) => {
+                  return Row(r, index);
+                })
+              ) : (
+                <tr>
+                  <td colSpan="5"
+                      className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500 text-center">
+                    No Logs
+                  </td>
+                </tr>
+              )}
               </tbody>
             </table>
           </div>
--- src/pages/setup/tracking.js
+++ src/pages/setup/tracking.js
@@ -56,51 +56,6 @@ function SetupTutorial() {
         in the documentation{' '}
       </p>
 
-      <div className="relative flex items-center justify-center mt-5">
-        <div className="flex items-center h-5">
-          <input
-            id="comments"
-            type="checkbox"
-            defaultChecked={track}
-            className="form-checkbox h-4 w-4 text-blue-600 transition duration-150 ease-in-out"
-            onChange={e => setTrack(e.target.checked)}
-          />
-        </div>
-        <div className="ml-3 text-sm leading-5">
-          <p className="text-gray-500">
-            I'm happy for you to track the version of Assistant Relay I use
-          </p>
-        </div>
-      </div>
-
-      <div className="relative flex items-center justify-center mt-5">
-        <div className="flex items-center h-5">
-          <a
-            href="https://www.facebook.com/assistantrelay"
-            target="_blank"
-            className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4
-            font-medium rounded-md text-white bg-blue-800 hover:bg-blue-700 focus:outline-none
-            focus:border-blue-900 focus:shadow-outline-blue active:bg-blue-900 transition ease-in-out duration-150"
-          >
-            <svg
-              className="w-4 h-4 mr-2"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"
-              />
-            </svg>
-            Like our Facebook Page
-          </a>
-        </div>
-      </div>
-
       <div className="flex mt-6">
         <div className="mx-auto">
           <a
